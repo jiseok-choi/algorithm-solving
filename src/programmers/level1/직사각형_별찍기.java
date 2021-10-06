7ae7a0c30b4c722d30d0bc5c93114b0d829d1654
jiseok-choi <choi300300@gmail.com>
2021-10-06 23:20:09 +0900

    Feat: 직사각형 별찍기 문제 풀이

---
+++ src/programmers/level1/직사각형_별찍기.java
@@ -0,0 +1,21 @@
+package programmers.level1;
+
+import java.util.Scanner;
+
+public class 직사각형_별찍기 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        // System.out.println(a + b);
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < a; i++) {
+            sb.append("*");
+        }
+        for (int i = 0; i < b; i++) {
+            System.out.println(sb);
+        }
+    }
+}
